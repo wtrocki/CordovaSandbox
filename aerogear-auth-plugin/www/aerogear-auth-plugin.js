c742a5c4aa6af5b98606e559079d793cff17fadf
Wojciech Trocki <wtrocki@redhat.com>
2018-02-15 20:08:45 +0000

    Try out plugin

--- aerogear-auth-plugin/www/aerogear-auth-plugin.js
+++ aerogear-auth-plugin/www/aerogear-auth-plugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'aerogear-auth-plugin', 'coolMethod', [arg0]);
+exports.auth = function (arg0, success, error) {
+    exec(success, error, 'aerogear-auth-plugin', 'auth', [arg0]);
 };
